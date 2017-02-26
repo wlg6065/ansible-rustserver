8a804dcad3ab6da025b6a6097a04e854cd2399b0
Doom Crickets Admin <dcadmin@lnx1.doomcrickets.com>
2017-02-26 06:02:12 -0500

    Update wipe routine to keep configuration files, that way we keep bans across wipes

---
+++ scripts/deltree-except.ps1
@@ -0,0 +1,9 @@
+param (
+    [string]$path,
+    [string]$exclude = " "
+)
+Get-ChildItem -Path $path -Recurse |
+Select -ExpandProperty FullName |
+Where {$_ -NotMatch $exclude } |
+sort length -Descending |
+Remove-Item -force
